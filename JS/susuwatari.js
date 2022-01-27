4813ca6515b085cfbddacc6902fc6d57dc38e129
Vicra2501 <Raphaelsonntag@icloud.com>
2022-01-27 15:26:31 +0100

    0.9 -susu moving up and down

--- JS/susuwatari.js
+++ JS/susuwatari.js
@@ -1,30 +1,41 @@
 class Susuwatari {
     constructor(image) {
         this.image = image
-        this.x =  width  
+        this.x = width
         // sets different starting points heightwise
-        this.y = (Math.random(height - 100, width - 100) * height) 
+        this.y = (Math.random(height - 100, width - 100) * height)
         this.width = 60
         this.height = 60
-        
+        this.num = random(-5, 5)
+        this.frequenzy = random(5, 7)
     }
 
-    
-// draws a target accros the canvas
+
+    // draws a target accros the canvas
 
 
     draw() {
         // directio and speed of targets
         this.x = this.x - 5
         // up and down movement of target
-         if(frameCount % 5 === 0 || frameCount % 60 <= 30 ){
-        this.y = this.y + random(0, 5)
-        }
-        else if(frameCount % 5 === 0 || frameCount % 60 >= 30 ){
-            this.y = this.y + random(-5 , 0)
+        // let num = random(1,10)
+        //  if( num % 2 == 0 ){
+        //      num = -num
+        //      console.log(num)
+
+        //     } 
+        this.y = this.y + (this.num)
+        if (frameCount % 30 <= Math.random()) {
+            this.num = -this.num 
+            if (frameCount % 9 == 0){
+                this.num + random(0,50)
             }
-        
-        image(this.image, this.x, this.y, this.width, this.height)
+        }
+            /*      else if( frameCount % 60 >= Math.floor(Math.random() * 60) ){
+                  this.y = this.y + random(-1, -100)
+                      } */
+
+            image(this.image, this.x, this.y, this.width, this.height)
     }
 
     //.b
@@ -38,24 +49,24 @@ class Susuwatari {
     // verticalMovement = verticalMovement + random(0, 25)
 
     // collision(playerInfo) {
-	// 	// here the susuwatari detects a collision with the sugarstar
-	// 	// the middle of the obstacle
-	// 	let targetX = this.x + this.width / 2
-	// 	let targetY = this.y + this.height / 2
-	// 	// the middle of the player
-	// 	let sugarX = playerInfo.x + playerInfo.width / 2
-	// 	let sugarY = playerInfo.y + playerInfo.height / 2
-	// 	// console.log('collision', playerInfo)
-	// 	if (dist(targetX, targetY, sugarX, sugarY) > 15) {
-	// 		return false
-	// 	} else {
-	// 		// change the score
-	// 		game.score += 1
-	// 		console.log(game.score)
-	// 		return true
-	// 	}
-	// }
-  
+    // 	// here the susuwatari detects a collision with the sugarstar
+    // 	// the middle of the obstacle
+    // 	let targetX = this.x + this.width / 2
+    // 	let targetY = this.y + this.height / 2
+    // 	// the middle of the player
+    // 	let sugarX = playerInfo.x + playerInfo.width / 2
+    // 	let sugarY = playerInfo.y + playerInfo.height / 2
+    // 	// console.log('collision', playerInfo)
+    // 	if (dist(targetX, targetY, sugarX, sugarY) > 15) {
+    // 		return false
+    // 	} else {
+    // 		// change the score
+    // 		game.score += 1
+    // 		console.log(game.score)
+    // 		return true
+    // 	}
+    // }
+
     //   // the middle of the pointer
     //   let playerX = playerInfo.x = playerInfo.width / 2
     //   let playerY = playerInfo.y = playerInfo.heigth / 2
@@ -65,10 +76,10 @@ class Susuwatari {
     //   } else {
     //       return true
     //   }
-  
+
 
     //   draw() {
     //       this.x--
     //       this.x++
     //       image(this.image, this.x, this.y, this.width, this.heigth)
-   } 
+} 
